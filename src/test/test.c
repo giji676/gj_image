f778de51c2e2c42ebfa6df77c2717e3d4495c544
giji676 <tvtengoxachidze@gmail.com>
2026-06-02 23:58:56 +0100

    testing speeds of large png file parsing (very slow)

--- src/test/test.c
+++ src/test/test.c
@@ -2,19 +2,31 @@
 #include "gj_image/gj_image.h"
 #include <stdio.h>
 
-int test_image(const char *filename) {
+int test_image(const char *filename, int display) {
     int width, height, channels;
     unsigned char *data = gj_image_load(filename, &width, &height, &channels);
     if (!data) {
         printf("Err: %s", gj_get_last_error());
-        return -1; }
-    // display_image(data, width, height, channels);
+        return -1;
+    }
+
+    if (display) display_image(data, width, height, channels);
+
     gj_image_free(data);
     return 0;
 }
 
 int main() {
-    test_image("assets/cv.bmp");
-    // test_image("assets/anaconda.png");
+    // test_image("assets/cv.bmp");
+    // real    0m0.950s
+    // user    0m0.946s
+    // sys     0m0.003s
+    for (int i = 0; i < 1; i++) {
+        test_image("/home/giji/Programming/game-2/assets/backpack/diffuse.png", 1);
+    }
+    for (int i = 0; i < 1; i++) {
+        test_image("/home/giji/Programming/game-2/assets/backpack/specular.png", 1);
+    }
+
     return 0;
 }
